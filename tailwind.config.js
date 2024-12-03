f8fda844d5395cab08bf8b3198135196b8a69044
officialmuneebahmad <icsf23m@gmail.com>
2024-12-03 18:01:39 +0500

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}", // Adjust paths to match your project structure
     "./index.html",
+    "./laptops/mac/mac_2016.html",
+    "./laptops/mac/mac_2016_2.html",
+    "./laptops/mac/mac_2017.html",
+    "./laptops/mac/mac_2017_2.html",
+    "./laptops/mac/mac_2017_3.html",
   ],
   theme: {
     extend: {
