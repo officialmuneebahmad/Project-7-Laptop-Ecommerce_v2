bf35bcdb6382e4dff17c3543bde307a7d7e9ccf7
officialmuneebahmad <icsf23m@gmail.com>
2025-01-11 22:58:50 +0500

    Page 2 Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}", // Adjust paths to match your project structure
     "./index.html",
+    "./pages/pg2/pg2.html",
     "./laptops/mac/mac_2016.html",
     "./laptops/mac/mac_2016_2.html",
     "./laptops/mac/mac_2016_3.html",
     "./laptops/mac/mac_2017.html",
     "./laptops/mac/mac_2017_2.html",
     "./laptops/mac/mac_2017_3.html",
+    "./laptops/mac/mac_2018.html",
+    "./laptops/mac/mac_2018_2.html",
+    "./laptops/mac/mac_2018_3.html",
+    "./laptops/mac/mac_2019.html",
+    "./laptops/mac/mac_2019_2.html",
+    "./laptops/mac/mac_2019_3.html",
   ],
   theme: {
     extend: {
